25862cd49f1daa9e2884c5c83c0e49ae58d4cc49
Rosa Corstjens <rosa.corstjens@hva.nl>
2017-05-07 12:17:39 +0200

    Only derived command classes have restrictions

--- DungeonRPG/DungeonRPG/Assets/Scripts/Command/Command.cs
+++ DungeonRPG/DungeonRPG/Assets/Scripts/Command/Command.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Command<T> where T : IControlable
+public class Command<T> 
 {
     public virtual void Execute(T controller, float input = 0)
     {
