6273df506007178e9bc267fe4ba6d47fa62c1028
Guus van Gend <guus.van.gend@hva.nl>
2017-05-07 11:56:41 +0200

    removed the if onhitcooldown from the battlestate as this has become redundant with the introduction of a recieved damage state

--- DungeonRPG/DungeonRPG/Assets/Scripts/StateMachine/EnemyStates/EnemyBattleState.cs
+++ DungeonRPG/DungeonRPG/Assets/Scripts/StateMachine/EnemyStates/EnemyBattleState.cs
@@ -13,30 +13,23 @@ public class EnemyBattleState : State<EnemyController>
     //called every frame
     public override void Execute(EnemyController agent)
     {
-        if (!agent.OnHitCooldown)
+        float distance = (agent.targetTransform.transform.position - agent.transform.position).magnitude;
+        float direction = Vector3.Dot((agent.targetTransform.position - agent.transform.position).normalized,
+            agent.transform.forward);
+
+        if (distance < agent.NoticeDistance && distance > agent.AttackDistance)
         {
-            float distance = (agent.targetTransform.transform.position - agent.transform.position).magnitude;
-            float direction = Vector3.Dot((agent.targetTransform.position - agent.transform.position).normalized,
-                agent.transform.forward);
+            agent.Rotate();
 
-            if (distance < agent.NoticeDistance && distance > agent.AttackDistance)
-            {
-                agent.Rotate();
-
-                agent.Move();
-            }
-            else if (distance <= agent.AttackDistance && direction > 0)
-            {
-                agent.FiniteStateMachine.SetState(agent.FiniteStateMachine.PossibleStates["Attack"]);
-            }
-            else
-            {
-                agent.FiniteStateMachine.SetState(agent.FiniteStateMachine.PossibleStates["Idle"]);
-            }
+            agent.Move();
+        }
+        else if (distance <= agent.AttackDistance && direction > 0)
+        {
+            agent.FiniteStateMachine.SetState(agent.FiniteStateMachine.PossibleStates["Attack"]);
         }
         else
         {
-            agent.FiniteStateMachine.SetState(agent.FiniteStateMachine.PossibleStates["RecievedDamage"]);
+            agent.FiniteStateMachine.SetState(agent.FiniteStateMachine.PossibleStates["Idle"]);
         }
         return;
     }
